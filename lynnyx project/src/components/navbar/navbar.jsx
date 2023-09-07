116d5b835c2e9446ea2ad6f1770c99820a722c3a
Henrich Tanis <henrichtanis@gmail.com>
2023-09-06 22:05:49 -0500

    added some code to the navbar and styled it

--- lynnyx project/src/components/navbar/navbar.jsx
+++ lynnyx project/src/components/navbar/navbar.jsx
@@ -1,17 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './navbar.css'
 
 function Navbar1() {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"> <img width="534" alt="Screenshot 2023-09-06 at 8 50 45 PM" src="https://github.com/Henrich77/lynnyx/assets/119962472/006c9e2e-e29f-42bf-b685-f576b44d2c39"></img></Navbar.Brand>
+      <Navbar >
+        <Container style={{marginLeft:"4rem", backgroundColor:"white"}}>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            
+            <Nav.Link href="#pricing">Book an appointment</Nav.Link>
+            <Navbar.Brand href="#home"> <img  className="logo" width="300vw" alt="Screenshot 2023-09-06 at 8 50 45 PM" src="https://github.com/Henrich77/lynnyx/assets/119962472/006c9e2e-e29f-42bf-b685-f576b44d2c39"></img></Navbar.Brand>
+            <Nav.Link href="#home">Services</Nav.Link>
+            <Nav.Link href="#features">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
